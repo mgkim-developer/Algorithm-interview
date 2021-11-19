6358ed75bbbc45c6ba9bd042364dd79f910d6ddb
mgkim-developer <mgkim.developer@gmail.com>
2021-11-19 11:31:26 +0900

    2021.11.19_(Python_Grammar) complete.

--- Python_Grammar.py
+++ Python_Grammar.py
@@ -1067,7 +1067,7 @@ print(result)
 # product는 permutations와 같이 리스트와 같은 iterable 객체에서 r개의 데이터를 뽑아 일렬로 나열하는 모든 경우(순열)를 계산한다.
 # 다만 원소를 중복하여 뽑는다. product 객체를 초기화 할 때는 뽑고자 하는 데이터의 수를 repeat 속성값으로 넣어준다.
 # product는 클래스이므로 객체 초기화 이후에는 리스트 자료형으로 변환하여 사용한다.
-# 리스트 ['A', 'B', 'C']에서 중복을 포함하여 2개 (r = 2)fmf Qhqdk skdufgksms ahems ruddnfmf cnffurgksms dPtlsms ekdmarhk rkxek.
+# 리스트 ['A', 'B', 'C']에서 중복을 포함하여 2개 (r = 2)를 뽑아 순서에 상관없이 나열하는 모든 경우를 출력하는 예시는 다음과 같다.
 
 from itertools import product
 
