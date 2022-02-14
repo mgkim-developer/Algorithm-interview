e29a153597653965880e27bd101976b6fa338e43
mgkim-developer <mgkim.developer@gmail.com>
2022-02-14 13:17:34 +0900

    2022.02.14_(5-8.py DFS예제)

--- 이코테 정리/5-8.py
+++ 이코테 정리/5-8.py
@@ -5,7 +5,7 @@ def dfs(graph, v, visited):
     print(v, end = ' ')
     # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
     for i in graph[v]: #인접한 노드를 순회할 리스트로 지정
-        if not visited[i]: # 인접한 노드 순회 리스트중, 만약 방문한 적이 없는 경우, 
+        if not visited[i]: # 인접한 노드 순회 리스트중, 만약 방문한 적이 없는 경우, 재귀를 호출한다.
             dfs(graph, i, visited) # 재귀 호출
 
 # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
