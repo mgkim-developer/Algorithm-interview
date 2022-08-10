f933165cc423cca594339d4f6863e59c36748178
mgkim-developer <mgkim.developer@gmail.com>
2022-08-10 20:41:30 +0900

    2022.08.10_(백준 2667번 리팩토링 DFS/BFS)

--- BAEKJOON/DFS와BFS/2667.py
+++ BAEKJOON/DFS와BFS/2667.py
@@ -3,7 +3,7 @@ n = int(input())
 
 # 2차원 리스트의 맵 정보 입력받기
 graph = []
-count_list = []
+
 for i in range(n):
     graph.append(list(map(int, input())))
 
@@ -12,9 +12,9 @@ def dfs(x, y):
     # 주어진 범위를 벗어나는 경우에는 즉시 종료
     if x <= -1 or x >= n or y <= -1 or y >= n:
         return False
-    count = 0
     # 현재 노드를 아직 방문하지 않았다면
     if graph[x][y] == 1:
+        global count    # 이게 출력 핵심 이었음. 리팩토링 완료.
         count = count + 1
         # 해당 노드 방문 처리
         graph[x][y] = 0
@@ -24,27 +24,23 @@ def dfs(x, y):
         dfs(x, y-1)
         dfs(x-1, y)
         dfs(x+1, y)
-        count_list.append(count)
         return True
-    # return False
+    return False
 
-count_result_list = []
-
+count = 0
+count_result = []
 # 모든 노드(위치)에 대하여 집 채우기
 result = 0
+
 for i in range(n):
     for j in range(n):
         # 현재 위치에서 DFS 수행
         if dfs(i, j) == True:
-            result += 1
-            # print(count_list)
-            count_result = 0
-            for k in range(len(count_list)):
-                count_result = count_result+1
-                count_list.pop()
-            count_result_list.append(count_result)
+            count_result.append(count)
+            result = result + 1
+            count = 0
 
-count_result_list.sort()
 print(result)
-for i in range(len(count_result_list)):
-    print(count_result_list[i])
+count_result.sort()
+for i in count_result:
+    print(i)
