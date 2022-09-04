da608e2afa2d6abb12cf0c750ad1364696a93fec
mgkim-developer <mgkim.developer@gmail.com>
2022-09-04 18:02:39 +0900

    2022.09.04_(백준 2467번 주석 수정, 이분탐색, 투 포인터)

--- BAEKJOON/이분 탐색/2467.py
+++ BAEKJOON/이분 탐색/2467.py
@@ -8,7 +8,7 @@ def binary_search(array):
     # 두 포인터가 가리키는 수의 합의 절댓값이 작은 것을 저장해서 비교하는데 이용할 것이므로 문제에 따라 가능한 최댓값으로 초기화.
     present_abs = 2000000000
 
-    # sub_abs를 비교해서 저장 현재 두용액의 특성값 합의 절댓값이 sub_abs보다 작다면 각각 result_l, result_r에 저장
+    # present_abs를 비교해서 저장 현재 두용액의 특성값 합의 절댓값이 sub_abs보다 작다면 각각 result_l, result_r에 저장
     result_l, result_r = 0, 0
 
     left = 0
