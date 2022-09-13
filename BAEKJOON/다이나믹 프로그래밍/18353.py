4ab5f758de3911c2642521ebe8e9eaea5d05dfdc
mgkim-developer <mgkim.developer@gmail.com>
2022-09-14 00:49:12 +0900

    2022.09.14_(백준 18353번 다이나믹 프로그래밍 LIS 직관적 이용 위해 입력 REVERSE)

--- BAEKJOON/다이나믹 프로그래밍/18353.py
+++ BAEKJOON/다이나믹 프로그래밍/18353.py
@@ -11,4 +11,4 @@ for i in range(1, n):
         if soldier[j] < soldier[i]:
             dp[i] = max(dp[i], dp[j] + 1)
 # 이 문제는 LIS(Longest Increasing Subsequence) 알고리즘을 적용하는 문제다.
-print(n-max(dp)) # 그러면 dp테이블에는 가장 긴 감소하는 부분 수열 길이가 저장되므로 문제 조건에 따라 (전체길이-가장긴부분수열길이)를 해준 값이 결과다.
+print(n-max(dp)) # 그러면 dp테이블에는 가장 긴 증가하는 부분 수열 길이가 저장되므로 문제 조건에 따라 (전체길이-가장긴부분수열길이)를 해준 값이 결과다.
