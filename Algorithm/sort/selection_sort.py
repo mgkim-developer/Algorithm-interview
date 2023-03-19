9895fb03b1000c8a7a8980e880cedf5326993e6c
mgkim-developer <mgkim.developer@gmail.com>
2023-03-20 03:22:43 +0900

    2023.03.20_(4. 정렬)

---
+++ Algorithm/sort/selection_sort.py
@@ -0,0 +1,11 @@
+# 선택 정렬을 사용하여 오름차순 정렬
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(arr)):
+    min_idx = i # 가장 작은 원소의 인덱스
+    for j in range(i + 1, len(arr)):
+        if arr[min_idx] > arr[j]:
+            min_idx = j
+    arr[min_idx], arr[i] = arr[i], arr[min_idx]
+
+print(arr)
